f3489f31e066f6483d575711e60795c222d4b031
Lucas Nørgård <lucasnrgaard@gmail.com>
2022-07-17 15:09:52 +0200

    chore: updated fastify

--- packages/fastify/src/index.ts
+++ packages/fastify/src/index.ts
@@ -1,9 +1,18 @@
 import type { FastifyPluginCallback } from 'fastify';
 import flugin from 'fastify-plugin';
 
-const plugin: FastifyPluginCallback = flugin((fastify, opts) => {}, {
-  name: 'my-plugin',
-  fastify: '3.x'
-});
+import type { ZoteraConfig } from '@zotera/types';
+
+type PluginOptions = Omit<ZoteraConfig, 'logging'>;
+
+const plugin: FastifyPluginCallback<PluginOptions> = flugin(
+  (fastify, opts) => {
+    console.log('Hello from plugin');
+  },
+  {
+    name: 'zotera',
+    fastify: '3.x'
+  }
+);
 
 export default plugin;
