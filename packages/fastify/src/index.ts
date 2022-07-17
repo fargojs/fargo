7548b43514252e09cf8f2f2274b627b5ccb28128
Lucas Nørgård <lucasnrgaard@gmail.com>
2022-07-17 15:00:21 +0200

    chore: formatted

--- packages/fastify/src/index.ts
+++ packages/fastify/src/index.ts
@@ -1,11 +1,9 @@
 import type { FastifyPluginCallback } from 'fastify';
 import flugin from 'fastify-plugin';
 
-const plugin: FastifyPluginCallback = flugin((fastify, opts) => {
-  
-}, {
+const plugin: FastifyPluginCallback = flugin((fastify, opts) => {}, {
   name: 'my-plugin',
-  fastify: '3.x',
+  fastify: '3.x'
 });
 
 export default plugin;
--- packages/server/src/web/routes/index.ts
+++ packages/server/src/web/routes/index.ts
@@ -7,7 +7,7 @@ import { web } from './web';
 export const routes: FastifyPluginCallback<FastifyPluginOptions> = (app, options, done) => {
   app.register(ping);
   app.register(extension);
-  
+
   if (options.config.web?.enabled) {
     app.log.info('Web server enabled');
     app.register(web);
