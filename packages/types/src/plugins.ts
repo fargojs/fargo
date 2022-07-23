3d50d81bcacafd078b5e4136e730589767d52a1d
luxass <lucasnrgaard@gmail.com>
2022-07-23 13:37:10 +0200

    Plugin API

--- packages/types/src/plugins.ts
+++ packages/types/src/plugins.ts
@@ -1,9 +1,23 @@
+import type { ZoteraAuth } from './auth';
 import type { ZoteraStorage } from './storage';
 
 export interface PluginContext {
+  log: ZoteraLoggingContext;
+  auth: ZoteraAuthContext;
   storage: ZoteraStorageContext;
+}
+
+interface ZoteraLoggingContext {
+  info(message: string): void;
+  warn(message: string): void;
+  error(message: string): void;
+  debug(message: string): void;
 }
 
 interface ZoteraStorageContext {
   register(storage: ZoteraStorage): void;
+}
+
+interface ZoteraAuthContext {
+  register(auth: ZoteraAuth): void;
 }
