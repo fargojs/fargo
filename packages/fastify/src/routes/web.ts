3fcf629d761ea4149ce9e9e6440a0374ae1a4185
luxass <lucasnrgaard@gmail.com>
2022-08-12 23:08:56 +0200

    chore: config is now working correctly

--- packages/fastify/src/routes/web.ts
+++ packages/fastify/src/routes/web.ts
@@ -1,6 +1,15 @@
 import type { FastifyInstance } from 'fastify';
 
+import { MimeTypes } from '@zotera/core';
+
 export async function web(zotera: FastifyInstance) {
+  // This route should only be activated in development mode.
+  // because in prod, we use the web options attached to window object.
+  // This route is used to fetch the web options from the server.
+  zotera.get('/__zotera_web_options__', async (_, res) => {
+    res.type(MimeTypes.JSON).send(zotera.config.web);
+  });
+
   zotera.get('/', async (req, res) => {
     res.type('text/html').send(/* html */ `
     <!DOCTYPE html>
--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,17 +1,22 @@
+import got from 'got';
 import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
 
+export default defineConfig(async ({ mode }) => {
+  const options = await got('http://localhost:4000/__zotera_web_options__').json();
 
-
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '^/-/.*': {
-        target: 'http://localhost:4000'
+  return {
+    plugins: [react()],
+    define: {
+      'window.__ZOTERA_WEB_OPTIONS': options
+    },
+    server: {
+      proxy: {
+        '^/-/.*': {
+          target: 'http://localhost:4000'
+        }
       }
     }
-  }
+  };
 });
