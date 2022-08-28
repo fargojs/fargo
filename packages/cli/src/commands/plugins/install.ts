2e4c8d645dda61ff3000e93ba24603735b70aff7
luxass <lucasnrgaard@gmail.com>
2022-08-28 21:59:37 +0200

    install command initial

--- packages/cli/src/commands/plugins/install.ts
+++ packages/cli/src/commands/plugins/install.ts
@@ -1,11 +1,12 @@
 import { Command } from 'commander';
 
-export const get = new Command('get')
-  .description('Download a plugin')
+export const install = new Command('install')
+  .description('install plugin(s)')
   .option('-d, --dir <dir>', 'Directory to download to')
+  .option('-c, --config <path>', 'Configuration file path')
   .argument('<plugins...>', 'Plugin(s) to download')
   .action((plugins) => {
     console.log('This is not implemented yet.');
-    console.log(JSON.stringify(get.opts().dir, null, 2));
+    console.log(JSON.stringify(install.opts().dir, null, 2));
     console.log(JSON.stringify(plugins, null, 2));
   });
