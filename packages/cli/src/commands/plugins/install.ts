a83cccec5aa472a33f03ac2406a96f74798f92e0
Lucas Nørgård <lucasnrgaard@gmail.com>
2022-08-31 13:54:50 +0200

    small changes to install

--- packages/cli/src/commands/plugins/install.ts
+++ packages/cli/src/commands/plugins/install.ts
@@ -1,8 +1,12 @@
+import axios from 'axios';
 import { Command } from 'commander';
-import path from 'path';
 import fs from 'fs';
+import path from 'path';
+
 import { readConfiguration } from '@zotera/config';
 import { unpack } from '@zotera/core';
+
+const allowedProtocols = new Set(['http', 'https']);
 
 export const install = new Command('install')
   .description('install plugin(s)')
@@ -12,18 +16,31 @@ export const install = new Command('install')
     const { pluginDir, __location } = readConfiguration(install.parent?.parent?.opts().config);
 
     const baseDownloadDir = path.join(__location, pluginDir);
-    plugins.forEach((plugin) => {
+    plugins.forEach(async (plugin) => {
       console.log(`Downloading ${plugin} to ${baseDownloadDir}`);
 
       if (path.extname(plugin) === '.zop') {
-        if (!fs.existsSync(baseDownloadDir)) {
-          fs.mkdirSync(baseDownloadDir);
-          
+        if (fs.existsSync(plugin)) {
+          unpack(plugin, path.join(baseDownloadDir, plugin.replace(/\.zop$/, '')));
+          return;
         }
-        // File should exists on disk.
-        unpack(plugin, path.join(baseDownloadDir, plugin.replace(/\.zop$/, '')));
+
+        try {
+          const { protocol, href } = new URL(plugin);
+          if (!allowedProtocols.has(protocol)) {
+            throw new Error(`Protocol ${protocol} is not allowed`);
+          }
+          const response = await axios.get(href, {
+            responseType: 'stream'
+          });
+
+/*           const pf = response.data.pipe(fs.createWriteStream(pathFile));
+          pf.on('finish', () => {
+            console.log('File downloaded successfully :)');
+          }); */
+        } catch (e) {
+          console.error(e);
+        }
       }
-
-
     });
   });
