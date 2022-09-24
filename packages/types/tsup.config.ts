8686583f2e73ba4aa061a2fb461554c475c06c01
Lucas Nørgård <lucasnrgaard@gmail.com>
2022-09-24 16:10:21 +0200

    tested typeschema

--- packages/types/tsup.config.ts
+++ packages/types/tsup.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'tsup';
+import PLUGIN from 'typeschema/plugin ';
 
 export default defineConfig({
   entry: ['src/types.ts', 'src/api.ts'],
   clean: true,
   dts: {
     only: true
-  }
+  },
+  esbuildPlugins: [
+    PLUGIN({
+      json: {
+        input: ['src/*.ts'],
+        outputDir: '.',
+        tsconfig: 'tsconfig.json'
+      }
+    })
+  ]
 });
